07a064d327bb3c5a88b2b3ff16f0d3baea44ee96
Deepak Chhantyal <deepakchhantyal4156@gmail.com>
2025-07-06 21:01:39 +0545

    feat: add SplashCursor component for fluid simulation and rendering
    - Implemented SplashCursor component with WebGL for fluid dynamics.
    - Added shaders for rendering and simulating fluid behavior.
    - Integrated pointer events for user interaction with the fluid simulation.
    
    style: add Tailwind CSS for styling
    
    - Imported Tailwind CSS in index.css for utility-first styling.
    
    chore: setup main entry point and Vite configuration
    
    - Created main.jsx as the entry point for the application.
    - Configured Vite with React and Tailwind CSS plugins.

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import { NavbarDefault } from './components/Navbar/Navbar'
+import SplashCursor from './components/SplashCursor/SplashCursor'
+
+function App() {
+
+
+  return (
+    <>
+    <div>
+      <SplashCursor />
+      <div>
+          <NavbarDefault />
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
