e0ae76c148e48d5bce6c1751723c85440498767c
Deepak Chhantyal <deepakchhantyal4156@gmail.com>
2025-07-06 21:48:56 +0545

    feat: implement HeroSection component and integrate into Home page

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,22 @@
 import './App.css'
 import { NavbarDefault } from './components/Navbar/Navbar'
 import SplashCursor from './components/SplashCursor/SplashCursor'
+import Home from './Pages/Home/Home'
 
 function App() {
 
 
   return (
     <>
-    <div>
-      <SplashCursor />
       <div>
+        <SplashCursor />
+        <div>
           <NavbarDefault />
+          <div>
+            <Home />
+          </div>
+        </div>
       </div>
-    </div>
     </>
   )
 }
